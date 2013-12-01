99cf163b184630959b96d173787a3ed083d2911d
raptor2101 <raptor2101@gmx.de>
2013-12-01 22:37:17 +0100

    ADD: MessageListing CHG: adopt changes from the referenced libraries CHG: change display of the title

--- src/de/raptor2101/BattleWorldsKronos/Connector/Gui/Activities/MessageListingActivity.java
+++ src/de/raptor2101/BattleWorldsKronos/Connector/Gui/Activities/MessageListingActivity.java
@@ -2,21 +2,19 @@ package de.raptor2101.BattleWorldsKronos.Connector.Gui.Activities;
 
 import android.os.Bundle;
 import android.view.Window;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import de.raptor2101.BattleWorldsKronos.Connector.Gui.R;
 
-public class GameListingActivity extends AbstractGameListingActivity implements OnItemClickListener{
+public class MessageListingActivity extends AbstractMessageListingActivity{
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    
     requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
     super.onCreate(savedInstanceState);
     getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.advanced_title_bar);
     
     TextView textView = (TextView) findViewById(R.id.advanced_title_bar_title);
-    textView.setText(R.string.title_activity_games);
+    textView.setText(this.getTitle());
   }  
 
   @Override
